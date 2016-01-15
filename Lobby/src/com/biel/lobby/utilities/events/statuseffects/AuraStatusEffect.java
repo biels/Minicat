94f804c1b8f179f94ec6723e20a2e1691d35323e
Biel Simon Rojas <bielsimon2@gmail.com>
2016-01-15 14:56:41 +0100

    + Added aura status effect class

---
+++ Lobby/src/com/biel/lobby/utilities/events/statuseffects/AuraStatusEffect.java
@@ -0,0 +1,24 @@
+package com.biel.lobby.utilities.events.statuseffects;
+
+import org.bukkit.entity.Player;
+
+public class AuraStatusEffect extends StatusEffect {
+
+	public AuraStatusEffect(Player ply) {
+		super(ply);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
