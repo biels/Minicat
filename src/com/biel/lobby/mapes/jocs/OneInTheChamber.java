5556ae64436bd50352a674f0c5042f41995b2c0d
amiguet3 <amiguet06@gmail.com>
2016-07-17 18:23:05 +0200

    afegit nou mode de joc OneInTheChamber

--- src/com/biel/lobby/mapes/jocs/OneInTheChamber.java
+++ src/com/biel/lobby/mapes/jocs/OneInTheChamber.java
@@ -1,5 +1,15 @@
 package com.biel.lobby.mapes.jocs;
 
+import java.util.ArrayList;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.Potion;
+import org.bukkit.potion.PotionType;
+
 import com.biel.lobby.mapes.JocScoreRace;
 
 public class OneInTheChamber extends JocScoreRace {
@@ -7,13 +17,38 @@ public class OneInTheChamber extends JocScoreRace {
 	@Override
 	protected int getFinishScore() {
 		// TODO Auto-generated method stub
-		return 0;
+		return Math.min(25, 10 + getPlayers().size() * 2);
 	}
 
 	@Override
 	public String getGameName() {
 		// TODO Auto-generated method stub
-		return null;
+		return "OneInTheChamber";
+	}
+	@Override
+	protected ArrayList<ItemStack> getStartingItems(Player ply){
+		ArrayList<ItemStack> items = new ArrayList<ItemStack>();
+		items.add(new ItemStack(Material.IRON_SWORD));
+		items.add(new ItemStack(Material.BOW));
+		items.add(new ItemStack(Material.ARROW));
+		@SuppressWarnings("deprecation")
+		Potion p1 = new Potion(PotionType.INSTANT_DAMAGE);
+		p1.setSplash(true);
+		items.add(p1.toItemStack(1));
+		return items;
+	}
+	protected void onPlayerDeathByPlayer(PlayerDeathEvent evt, Player killed,
+			Player killer) {
+		super.onPlayerDeathByPlayer(evt, killed, killer);
+		incrementScore(killer);
+		ItemStack Arrow = new ItemStack(Material.ARROW);
+		getStartingItems(killed);
+		killer.getInventory().addItem(Arrow);
+	}
+	protected void onPlayerDamageByPlayer(EntityDamageByEntityEvent evt, Player damaged, Player damager,
+			boolean ranged){
+		super.onPlayerDamageByPlayer(evt, damaged, damager, ranged);
+		if(ranged)damaged.damage(1000);
 	}
 
 }
