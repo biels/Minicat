ba8edd234ffc3fc90dd02ea0debbe19c7633bc68
lluiscab <lluiscab@gmail.com>
2018-08-11 20:55:44 +0200

    One in the chamber - Fixes #85 (#87)
    * Fixes #85

--- src/main/java/com/biel/lobby/mapes/jocs/OneInTheChamber.java
+++ src/main/java/com/biel/lobby/mapes/jocs/OneInTheChamber.java
@@ -22,15 +22,14 @@ public class OneInTheChamber extends JocScoreRace {
 
 	@Override
 	protected int getFinishScore() {
-		// TODO Auto-generated method stub
 		return Math.min(25, 10 + getPlayers().size() * 2);
 	}
 
 	@Override
 	public String getGameName() {
-		// TODO Auto-generated method stub
 		return "OneInTheChamber";
 	}
+
 	@Override
 	protected void teletransportarTothom() {
 		for (Player d : getPlayers()) {  // d gets successively each value in ar.
@@ -47,9 +46,15 @@ public class OneInTheChamber extends JocScoreRace {
 	private Location getRandomSpawnLoc() {
 		Location loc;
 		ArrayList<Location> locs = pMapaActual().ObtenirLocations("arena", world);
+
+		if(locs.size() == 0) {
+			return new Location(world, -2343, 62, 647);
+		}
+
 		loc = new Location(world, Utils.NombreEntre(locs.get(0).getBlockX(), locs.get(1).getBlockX()), locs.get(0).getY() + 1, Utils.NombreEntre(locs.get(0).getBlockZ(), locs.get(1).getBlockZ()));
 		return loc;
 	}
+
 	@Override
 	protected ArrayList<ItemStack> getStartingItems(Player ply){
 		ArrayList<ItemStack> items = new ArrayList<>();
@@ -68,19 +73,23 @@ public class OneInTheChamber extends JocScoreRace {
 		items.add(p1.toItemStack(1));
 		return items;
 	}
-	protected void onPlayerDeathByPlayer(PlayerDeathEvent evt, Player killed,
-			Player killer) {
+
+	protected void onPlayerDeathByPlayer(PlayerDeathEvent evt, Player killed, Player killer) {
+
 		super.onPlayerDeathByPlayer(evt, killed, killer);
 		incrementScore(killer);
 		ItemStack Arrow = new ItemStack(Material.ARROW);
-		getStartingItems(killed);
 		killer.getInventory().addItem(Arrow);
 	}
-	protected void onPlayerDamageByPlayer(EntityDamageByEntityEvent evt, Player damaged, Player damager,
-			boolean ranged){
+
+	// Instant kill potion I guess
+	protected void onPlayerDamageByPlayer(EntityDamageByEntityEvent evt, Player damaged, Player damager, boolean ranged) {
+
 		super.onPlayerDamageByPlayer(evt, damaged, damager, ranged);
-		if(ranged)damaged.damage(1000);
+		if (ranged) evt.setDamage(100);
+
 	}
+
 	@Override
 	protected void onPlayerRespawnAfterTick(PlayerRespawnEvent evt, Player p) {
 		super.onPlayerRespawnAfterTick(evt, p);
